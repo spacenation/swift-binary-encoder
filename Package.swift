6026ba01752342f80fdd46d2260829fe8cc37e19
Alexander Young <alex.buzov@icloud.com>
2022-07-17 10:43:45 -0700

    stabilization

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
             targets: ["BinaryEncoder"]),
     ],
     dependencies: [
-        .package(name: "Binary", url: "git@github.com:spacenation/swift-binary.git", from: "0.1.2"),
-        .package(name: "Functions", url: "git@github.com:spacenation/swift-functions.git", .upToNextMajor(from: "0.5.0")),
-        .package(name: "Currying", url: "git@github.com:spacenation/swift-currying.git", .upToNextMajor(from: "0.1.0")),
-        .package(name: "List", url: "git@github.com:spacenation/swift-list.git", .upToNextMajor(from: "0.3.3"))
+        .package(name: "Binary", url: "git@github.com:spacenation/swift-binary.git", .upToNextMajor(from: "1.0.0")),
+        .package(name: "Functions", url: "git@github.com:spacenation/swift-functions.git", .upToNextMajor(from: "1.0.0")),
+        .package(name: "Currying", url: "git@github.com:spacenation/swift-currying.git", .upToNextMajor(from: "1.0.0")),
+        .package(name: "List", url: "git@github.com:spacenation/swift-list.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
